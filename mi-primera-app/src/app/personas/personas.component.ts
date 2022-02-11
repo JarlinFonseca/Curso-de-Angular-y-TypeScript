4666900ca0f26bb567e60fe060edcfa7e66f2a7b
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-11 16:46:05 -0500

    Two Way Binding

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -10,15 +10,16 @@ import { Component } from "@angular/core";
  export class PersonasComponent{
      deshabilitar = false;
      mensaje = 'No se ha agregado ninguna persona';
-     titulo = '';
+     titulo = 'Ingeniero';
 
      agregarPersona(){
             this.mensaje = 'Persona agregada';
      }
-
-     modificarTitulo(event: Event){
+ 
+     //Event Binding
+   /*   modificarTitulo(event: Event){
         this.titulo = (<HTMLInputElement>event.target).value;
      }
-
+ */
 
  }
