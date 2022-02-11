74c1f21f52a7d751cae99faa027e977fc798c06c
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-11 16:33:51 -0500

    Event Binding Parte 2

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -10,9 +10,14 @@ import { Component } from "@angular/core";
  export class PersonasComponent{
      deshabilitar = false;
      mensaje = 'No se ha agregado ninguna persona';
+     titulo = '';
 
      agregarPersona(){
             this.mensaje = 'Persona agregada';
+     }
+
+     modificarTitulo(event: Event){
+        this.titulo = (<HTMLInputElement>event.target).value;
      }
 
 
