ed37b0d2b44aedc57e76f3b2a04ac68787538a46
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-15 09:36:40 -0500

    Creación clase Persona

---
+++ listado-personas/src/app/persona.model.ts
@@ -0,0 +1,11 @@
+export class Perona{
+   /*  nombre: string = '';
+    apellido: string = ''; 
+
+    constructor(nombre: string, apellido: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    } */
+
+    constructor(public nombre: string, public apellido: string){}
+}
