db7b7b3dd75563193b414d10de8d5d811f85adee
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-17 09:24:27 -0500

    Property Binding entre componentes

--- listado-personas/src/app/app.module.ts
+++ listado-personas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule, FormsModule
