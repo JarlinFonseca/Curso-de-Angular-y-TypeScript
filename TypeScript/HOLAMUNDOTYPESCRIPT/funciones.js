dda68b428949556f0877cce4416bbf804cec2bf8
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-17 16:32:47 -0500

    Definición de Variables con TypeScript

--- TypeScript/HOLAMUNDOTYPESCRIPT/funciones.js
+++ TypeScript/HOLAMUNDOTYPESCRIPT/funciones.js
@@ -1,3 +1,16 @@
 "use strict";
-const saludar = () => console.log("saludar adios");
+//let saludo:string = 'HolaMundo desde TypeScript';
+let saludo = 'HolaMundo desde TypeScript';
+saludo = "cambio";
+let numero = 10;
+let cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+const PI_NUMERO = 3.1416;
+const saludar = () => {
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+    console.log(PI_NUMERO);
+};
 saludar();
--- TypeScript/HOLAMUNDOTYPESCRIPT/funciones.ts
+++ TypeScript/HOLAMUNDOTYPESCRIPT/funciones.ts
@@ -1,3 +1,21 @@
-const saludar =  () => console.log("saludar adios");
+//let saludo:string = 'HolaMundo desde TypeScript';
+let saludo = 'HolaMundo desde TypeScript';
+
+saludo = "cambio";
+
+let numero:number = 10;
+
+let cualquiera:any;
+cualquiera = "cadena";
+cualquiera = 10;
+
+const PI_NUMERO = 3.1416;
+
+const saludar =  () => {
+    console.log(saludo); 
+    console.log(numero) 
+    console.log(cualquiera); 
+    console.log(PI_NUMERO)
+}
 
 saludar();
