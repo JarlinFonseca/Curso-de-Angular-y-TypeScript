d7d7c155beb938a46ab5fd6473c7a3c249213e5f
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-19 08:01:22 -0500

    Event Binding entre Componentes

--- Angular/listado-personas/src/app/app.component.ts
+++ Angular/listado-personas/src/app/app.component.ts
@@ -15,11 +15,8 @@ export class AppComponent {
     new Persona('Karla', 'Lara')
   ];
 
-  nombreInput: string = '';
-  apellidoInput: string = '';
-
-  agregarPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personas.push(persona1);
+  personaAgregada(persona: Persona){
+    this.personas.push(persona);
   }
+
 }
--- Angular/listado-personas/src/app/app.module.ts
+++ Angular/listado-personas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PersonaComponent } from './persona/persona.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonaComponent
+    PersonaComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule, FormsModule
